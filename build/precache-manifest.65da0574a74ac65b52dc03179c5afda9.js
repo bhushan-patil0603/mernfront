e24dc2a73d8e076ca4f0780165e3b9d60bd78d20
bhushan <pbhushan0603@gmail.com>
2024-11-26 00:18:20 +0530

    Updated start script and added static.json

---
+++ build/precache-manifest.65da0574a74ac65b52dc03179c5afda9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984f6903eec82f81a2bec2176824d9f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64cba73adbd3ba068bda",
+    "url": "/static/css/main.39560e91.chunk.css"
+  },
+  {
+    "revision": "e51e7632ded1c0fb5f3b",
+    "url": "/static/js/2.7243d044.chunk.js"
+  },
+  {
+    "revision": "4150a627f5138970747231fcef63f425",
+    "url": "/static/js/2.7243d044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64cba73adbd3ba068bda",
+    "url": "/static/js/main.4f170c96.chunk.js"
+  },
+  {
+    "revision": "c997bd8de876113cda4d",
+    "url": "/static/js/runtime-main.1ce35d81.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
